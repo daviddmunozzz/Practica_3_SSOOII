41305eb7fef316306b4eb9a0bf9ad99587afdcf3
daviddmunozzz <davidescribano2002@gmail.com>
2024-04-17 17:10:11 +0200

    Vacio

--- src/planificador.h
+++
@@ -1,9 +0,0 @@
-/*********************************************************************
-* Class Name: planificador.h
-* Author/s name: Daviz Muñoz y Daniel Aguado.
-* Release/Creation date: 10/4/2024
-* Class version: 1.0
-* Class description: 
-*
-**********************************************************************
-*/
